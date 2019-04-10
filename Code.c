434a9a0e7c7ef8a2c333e12bf65b51cecb992baa
Rahul Verma <rahul.verma.30.12.20@gmail.com>
2019-04-10 18:55:09 +0530

    Update Code.c

--- Code.c
+++ Code.c
@@ -10,61 +10,61 @@ sem_t semaphore;
 int size;
 pthread_mutex_t mutex;
 pthread_mutex_t mutex1;
-int main()
+void main()
 {
-    int burst_time[20],B[20],C[20],D[20] ,i ,j,total=0,pos,temp;
+    int A[20],B[20],C[20],D[20] ,i ,j ,n  , total=0,pos,temp;
     float avg_C,avg_D;
     printf("Enter number of process:");
-    scanf("%d",&size);
+    scanf("%d",&n);
  
     printf("\nEnter Burst Time:\n");
-    for(i=0;i<size;i++)
+    for(i=0;i<n;i++)
     {
         printf("p%d:",i+1);
-        scanf("%d",&burst_time[i]);
+        scanf("%d",&A[i]);
         B[i]=i+1;           
     }
 
-    for(i=0;i<size;i++)
+    for(i=0;i<n;i++)
     {
         pos=i;
-        for(j=i+1;j<size;j++)
+        for(j=i+1;j<n;j++)
         {
-            if(burst_time[j]<burst_time[pos])
+            if(A[j]<A[pos])
                 pos=j;
         }
  
-        temp=burst_time[i];
-        burst_time[i]=burst_time[pos];
-        burst_time[pos]=temp;
+        temp=A[i];
+        A[i]=A[pos];
+        A[pos]=temp;
  
-        temp=burst_time[i];
-        B[i]=B[pos];
-        B[pos]=temp;
+        temp=B[i];
+        B [i]=B[pos];
+        B [pos]=temp;
     }
  
     C[0] = 0;
-    for(i=1;i<size;i++)
+    for(i=1;i<n;i++)
     {
         C[i]=0;
         for(j=0;j<i;j++)
-            C[i]+=burst_time[j];
+            C[i]+=A[j];
  
         total+=C[i];
     }
-  avg_C=(float)total/size;      
+  avg_C=(float)total/n;      
      total=0;
  
     printf("\nProcess\t    Burst Time    \tWaiting Time\tTurnaround Time");
-    for(i=0;i<size;i++)
+    for(i=0;i<n;i++)
     {
-        D[i]=burst_time[i]+C[i];     //calculate turnaround time
+        D[i]=A[i]+C[i];     //calculate turnaround time
         total+=D[i];
-        printf("\np%d\t\t  %d\t\t    %d\t\t\t%d", B[i],burst_time[i],C[i],D[i]);
+        printf("\np%d\t\t  %d\t\t    %d\t\t\t%d", B[i],A[i],C[i],D[i]);
     }
  
-    avg_D=(float)total/size;     
+    avg_D=(float)total/n;     
     printf("\n\nAverage Waiting Time=%f",avg_C);
     printf("\nAverage Turnaround Time=%f\n",avg_D);
-    return 0;
 }
+
